311466c50c338d2f641be981342bdc77d4c2a69a
DimasWerito <krasilnykov@gmail.com>
2024-04-21 23:07:23 -0600

    ADDING EVEN NUMBERS

---
+++ day_5_3.py
@@ -0,0 +1,12 @@
+"""
+ADDING EVEN NUMBERS
+"""
+
+
+target = int(input("Enter a number between 0 and 1000\n")) # Enter a number between 0 and 1000
+
+even_sum = 0
+for number in range(2, target + 1, 2):
+  even_sum += number
+
+print(even_sum)
