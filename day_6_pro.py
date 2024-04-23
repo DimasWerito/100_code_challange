8b9960f23b7dcb5759804e1c8c9b7d568adff20a
DimasWerito <krasilnykov@gmail.com>
2024-04-22 19:18:32 -0600

    GCD

---
+++ day_6_pro.py
@@ -0,0 +1,14 @@
+"""
+GCD
+"""
+
+
+first = int(input("Input the first number>>>"))
+second = int(input("Input the second number>>>"))
+
+gcd = min(first, second)
+
+while not (first % gcd == 0 and second % gcd == 0):
+    gcd -= 1
+
+print(gcd)
