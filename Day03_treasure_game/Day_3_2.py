299817f66b52d20463a6cb2fb91b250c950f6fb5
DimasWerito <krasilnykov@gmail.com>
2024-04-24 16:44:20 -0600

    day 8

--- Day03_treasure_game/Day_3_2.py
+++ Day03_treasure_game/Day_3_2.py
@@ -6,17 +6,14 @@ BMI 2.0
 height = float(input("Enter your height in meters. "))
 # Enter your weight in kilograms e.g., 72
 weight = int(input("Enter your weight in kilograms. "))
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
-bmi = weight / (height * height)
+bmi = weight / (height * height) * 10000
 if bmi < 18.5:
-  print(f"Your BMI is {round(bmi, 2)}, you are underweight.")
+  print(f"Your BMI is {round(bmi)}, you are underweight.")
 elif bmi < 25:
-  print(f"Your BMI is {round(bmi, 2)}, you have a normal weight.")
+  print(f"Your BMI is {round(bmi)}, you have a normal weight.")
 elif bmi < 30:
-  print(f"Your BMI is {round(bmi, 2)}, you are slightly overweight.")
+  print(f"Your BMI is {round(bmi)}, you are slightly overweight.")
 elif bmi < 35:
-  print(f"Your BMI is {round(bmi, 2)}, you are obese.")
+  print(f"Your BMI is {round(bmi)}, you are obese.")
 else:
-  print(f"Your BMI is {round(bmi, 2)}, you are clinivally obese.")
+  print(f"Your BMI is {round(bmi)}, you are clinically obese.")
