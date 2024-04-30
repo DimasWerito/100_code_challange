a7fd06da0fba35013c07bffa38e2571c1800813d
DimasWerito <krasilnykov@gmail.com>
2024-04-30 12:57:47 -0600

    day 13

---
+++ day13_debugging/day_13_3.py
@@ -0,0 +1,17 @@
+"""
+LEAP YEAR
+"""
+
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
