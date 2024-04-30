9cd0447f7db5c2e2d147f3eb762b8d089def4c47
DimasWerito <krasilnykov@gmail.com>
2024-04-30 13:13:06 -0600

    day 13 finished

---
+++ day13_debugging/day_13_4.py
@@ -0,0 +1,15 @@
+"""
+FizzBuzz GAME
+"""
+
+
+target = int(input("What is your number? "))
+for number in range(1, target + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
