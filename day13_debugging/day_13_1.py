b69880a43e876ffed27171ac330d3227a7ca3e48
DimasWerito <krasilnykov@gmail.com>
2024-04-30 12:36:19 -0600

    day 13 finished

--- day13_debugging/day_13_1.py
+++ day13_debugging/day_13_1.py
@@ -6,19 +6,27 @@
 #     if i == 20:
 #       print("You got it")
 # my_function()
-
+"""
 # For loop doesnt include the last number, add 1
 def my_function():
   for i in range(1, 21):
     if i == 20:
       print("You got it")
 my_function()
+"""
 
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 # dice_num = randint(1, 6)
 # print(dice_imgs[dice_num])
+"""
+# index starts from 0 and takes number 6 by index 5, othewise you get an index error
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
+"""
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
@@ -26,11 +34,28 @@ my_function()
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
+"""
+# The year 1994 is not included in the if-statementm, use >=
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
+"""
 
 # # Fix the Errors
 # age = input("How old are you?")
 # if age > 18:
 # print("You can drive at age {age}.")
+"""
+# Fix the Errors
+# print must indented in the if-statement
+# input takes an a string, not an integer, use int() func
+# use f-string to print the age
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
+"""
 
 # #Print is Your Friend
 # pages = 0
@@ -39,6 +64,17 @@ my_function()
 # word_per_page == int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
+"""
+#variable word_per_page is not defined, use = to assign
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(f"pages = {pages}")
+print(f"word_per_page = {word_per_page}")
+print(total_words)
+"""
 
 # #Use a Debugger
 # def mutate(a_list):
@@ -49,3 +85,14 @@ my_function()
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
+"""
+#the b_list must be intended in the for loop
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
+"""
