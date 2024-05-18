4371991bfcaa75291f75ec82fc93cd85c07dd529
DimasWerito <krasilnykov@gmail.com>
2024-05-18 10:12:01 -0600

    day 26

--- day26_nato_alfa/list_dict_comprehension.py
+++ day26_nato_alfa/list_dict_comprehension.py
@@ -31,3 +31,16 @@ passed_students = {
     for (student, grade) in student_grades.items() if grade >= 60
 }
 print(passed_students)
+
+list_of_strings = input().split(',')
+# 🚨 Do  not change the code above
+
+# TODO: Use list comprehension to convert the strings to integers 👇:
+list_of_integers = [int(n) for n in list_of_strings]
+
+# TODO: Use list comprehension to filter out the odd numbers
+# and store the even numbers in a list called "result"
+result = [n for n in list_of_integers if n % 2 == 0]
+
+# Write your code 👆 above:
+print(result)
